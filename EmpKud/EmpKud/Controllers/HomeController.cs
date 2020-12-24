c0aaedd35322b6a32f79fc9f73ad6bde079c9967
Otokiti Ola <otokiti.tosin@gmail.com>
2020-12-24 08:09:34 +0200

    HomeController created, wwwroot with it's properties created

---
+++ EmpKud/EmpKud/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmpKud.Controllers
+{
+    public class HomeController : Controller
+    {
+        public JsonResult Index()
+        {
+            return Json(new { id = 1, name = "Merry Christmas" });
+        }
+    }
+}
--- EmpKud/EmpKud/Startup.cs
+++ EmpKud/EmpKud/Startup.cs
@@ -16,6 +16,8 @@ namespace EmpKud
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +29,10 @@ namespace EmpKud
             }
 
             app.UseRouting();
+
+            app.UseFileServer();
+
+            app.UseMvcWithDefaultRoute();
 
             app.UseEndpoints(endpoints =>
             {
